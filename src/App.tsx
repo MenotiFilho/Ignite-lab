6b54c776596f5ae875f40d1ddef1fcb14ef798de
MenotiFilho <menotimfilho@gmail.com>
2022-06-27 21:08:41 -0300

    Pagina video e roteamento feitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Sidebar } from "./components/Sidebar";
-import { Event } from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+import { client } from "./lib/apollo";
 
 function App() {
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 }
